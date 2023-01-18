ecb4b40e06fe4609a58f0b6235cb0a090a3c6428
Deimar <deimar.parn@voco.ee>
2023-01-18 15:07:07 +0000

    Implemented map offers that are marked as favorite

--- src/pages/user/Favorites.tsx
+++ src/pages/user/Favorites.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
+import {OfferInterface} from "../../Interfaces/interface";
 import './Favorites.css';
 import Nav from '../../Components/Nav/Nav';
 import Footer from '../../Components/Footer/Footer';
 import {FaHeart} from 'react-icons/fa';
 import {HiChevronLeft} from 'react-icons/hi';
 
-function Favorites({theme, handleThemeSwitch}: any) {
+function Favorites({theme, handleThemeSwitch, favorites}: any) {
 
     let navigate = useNavigate();
 
@@ -19,13 +20,39 @@ function Favorites({theme, handleThemeSwitch}: any) {
             <Nav theme={theme} handleThemeSwitch={handleThemeSwitch} />
             <div className="header-container">
                 <FaHeart id="favorite-icon" />
-                <h2>Favorites(3)</h2>
+                <h2 style={{color: theme ? 'white' : 'black'}}>
+                    Favorites({favorites.length})
+                </h2>
             </div>
-            <div className="nav-back-container">
+            <div className="nav-back-container" style={{color: theme ? 'white' : 'black'}}>
                <span onClick={handleNavigateBack}>
                     <HiChevronLeft id="back-icon" />
                     <h3>Go Back</h3>
                </span>
+            </div>
+            <div className="favorites-main-container">
+                {favorites.map((favorite: OfferInterface) =>{
+                    return (
+                        <div className="favorites-grid-item" key={favorite.id}>
+                            <div className="offer-image">
+                                <img src={favorite.image} alt="offer" />
+                            </div>
+                            <div className="offer-description-container">
+                                <div className="offer-description" style={{color: theme ? 'white' : 'black'}}>
+                                    <h3>Location:
+                                        <span> {favorite.location}</span>
+                                    </h3>
+                                    <h3>Description:
+                                        <span> {favorite.description}</span>
+                                    </h3>
+                                </div>
+                                <div className="offer-date-container">
+                                    <p>Date: {favorite.date}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
             <Footer theme={theme} />
         </div>
