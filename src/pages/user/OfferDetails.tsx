28c38231166d55fae80a98f3df3fb7034653f375
Deimar <deimar.parn@voco.ee>
2023-01-20 13:43:00 +0000

    Implemented show offer details & updated data json

--- src/pages/user/OfferDetails.tsx
+++ src/pages/user/OfferDetails.tsx
@@ -1,17 +1,24 @@
 import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 import './OfferDetails.css';
 import {OfferInterface} from "../../Interfaces/interface";
 import Nav from '../../Components/Nav/Nav';
 import Footer from '../../Components/Footer/Footer';
+import {FaRegHeart, FaHeart} from 'react-icons/fa';
+import {HiChevronLeft} from "react-icons/hi";
 
 function OfferDetails ({offersData, theme, handleThemeSwitch, favoriteCount}: any) {
 
+    const navigate = useNavigate();
     let { slug } = useParams();
 
     const offer = offersData.find((offer: OfferInterface) => offer.slug === slug);
 
     const [offerSelected, setOfferSelected] = useState<OfferInterface>({...offer});
+
+    const handleNavigateBack = () => {
+        navigate(-1);
+    }
 
     useEffect(() => {
         setOfferSelected({...offer});
@@ -24,6 +31,54 @@ function OfferDetails ({offersData, theme, handleThemeSwitch, favoriteCount}: an
     return (
         <div>
             <Nav theme={theme} handleThemeSwitch={handleThemeSwitch} favoriteCount={favoriteCount} />
+            <div className="details-nav-back-container" style={{color: theme ? 'white' : 'black'}}>
+                <span onClick={handleNavigateBack}>
+                    <HiChevronLeft id="back-icon" />
+                    <h3>Go Back</h3>
+                </span>
+            </div>
+            <div className="details-main-container">
+                <div className="details-grid-item">
+                    <div className="details-image">
+                        <img src={offerSelected.image} alt="offer" />
+                    </div>
+                    <div className="addToFavorites-container">
+                        <span onClick={handleNavigateBack}>
+                            <FaRegHeart id="fav-icon" />
+                            <h3>Add to favorites</h3>
+                         </span>
+                        <p>Date: {offerSelected.date}</p>
+                    </div>
+                    <div className="details-description">
+                        <h3>
+                            Title:<span> {offerSelected.title}</span>
+                        </h3>
+                        <h3>
+                            Category:<span> {offerSelected.category}</span>
+                        </h3>
+                        <h3>
+                            Location:<span> {offerSelected.location}</span>
+                        </h3>
+                        <h3>
+                            Description:<span> {offerSelected.description}</span>
+                        </h3>
+                        <h3>
+                            Price:<span> {offerSelected.price}€</span>
+                        </h3>
+                    </div>
+                </div>
+                <div className="details-feedback-container" style={{overflowY: offerSelected.feedback.length <= 2 ? 'hidden' : 'scroll'}}>
+                    <h2>Users Feedback</h2>
+                    {offerSelected.feedback.map((feedback: {comment: string, name: string}) => {
+                        return (
+                            <div className="user-feedback" key={feedback.comment}>
+                                <h3>{feedback.name}</h3>
+                                <p>{feedback.comment}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
             <Footer theme={theme} />
         </div>
     );
