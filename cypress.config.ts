891ae6e325ab2aa76ead2173bee8b8fe793d2d13
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-03-09 22:10:36 +0300

    Updated cypress e2e configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,9 @@
-export default {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 2000,
+    viewportHeight: 1200,
   },
-};
+})
