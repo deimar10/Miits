e4221292925ff280df39c9d942e12bc5c8293c49
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-03-28 19:16:50 +0300

    Updated enterprise delete offer e2e tests

--- cypress/e2e/enterprise/deleteOffer.cy.js
+++ cypress/e2e/enterprise/deleteOffer.cy.js
@@ -3,7 +3,7 @@ describe('Check that enterprise offers can be deleted from the table', () => {
         cy.visit('/enterprise/login');
     });
 
-    it('Deletes an offer from the table & checks if action modal is displayed', () => {
+    it('Deletes an offer from the table after confirmation & checks if action modal is displayed', () => {
         cy.get('input[name=username]').type('KolmTilli');
 
         cy.get('input[name=password]').type('Lollakas312456');
@@ -12,11 +12,13 @@ describe('Check that enterprise offers can be deleted from the table', () => {
 
         cy.get('[data-cy="nav-to-offers"]').click();
 
-        cy.get('#delete-icon').click();
+        cy.get('#offer-delete').click();
+
+        cy.get('#delete-submit').should('have.text', 'Kustuta').click();
 
         cy.get('[data-cy="offer-title"]').should('not.exist');
 
-        cy.get('[data-cy="action-description"]').should('contain.text', 'Pakkumine (109) edukalt eemaldatud');
+        cy.get('[data-cy="action-description"]').should('contain.text', 'Pakkumine (120) edukalt eemaldatud');
     });
 
 })
