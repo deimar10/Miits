bb6cd02be2b2a171557c55c36816bbdc20bb4312
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-07-12 15:32:28 +0300

    Updated interfaces

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -16,12 +16,14 @@ export interface OfferInterface {
         name: string
     }>
 }
+
 export interface feedback {
     name: string,
     comment: string,
     nameError: string,
     commentError: string
 }
+
 export interface register {
     username: string,
     password: string,
@@ -37,5 +39,22 @@ export interface registered {
     name: string,
     id: number,
     joined_at: string
+}
+
+export interface PublicRouteProps {
+    auth: {login: boolean},
+    setAuth: (auth: any) => void,
+    admin: boolean,
+    setAdmin: (admin: boolean) => void,
+    register: any
+    setRegister: any
+}
+
+export interface PrivateRouteProps {
+    theme: boolean,
+    auth: {login: boolean},
+    setAuth: (auth: {login: boolean}) => void,
+    handleThemeSwitch: () => void,
+    offersData: any
 }
 
